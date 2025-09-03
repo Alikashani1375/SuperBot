9664793e1e4010194f28ecfbf6627d69198a048f
AliKashani <57268571+Alikashani1375@users.noreply.github.com>
2025-09-03 13:19:52 +0330

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,12 @@
 "use client";
+
+import { Suspense } from "react";
 import LoginPageClient from "../../components/login/Login";
 
 export default function LoginPageWrapper() {
-  return <LoginPageClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginPageClient />
+    </Suspense>
+  );
 }
